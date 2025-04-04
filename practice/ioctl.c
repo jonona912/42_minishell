087c0ec00f07ee9ee99737685f9a26d4d62602b5
Oleksandr Popov <opopov@c2r3s2.42wolfsburg.de>
2025-04-04 17:56:08 +0200

    changes test 1

--- practice/ioctl.c
+++ practice/ioctl.c
@@ -1,10 +1,4 @@
 #include <sys/ioctl.h>
-#include <sys/socket.h>
-#include <net/if.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 
 int main() {
     int fd;
